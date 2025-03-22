b91ee8a8b31e7c556f8470de8c3737f6cec4bdab
nicholasereyes <61757591+nicholasereyes@users.noreply.github.com>
2025-03-22 02:23:11 +0000

    add raw table and brief description

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
+import math
+import numpy as np
+from pathlib import Path
 
-st.title("🎈 My new app")
+@st.cache_data
+def get_bach_data():
+    DATA_FILENAME = Path(__file__).parent/'data/bachelorette-contestants.csv'
+    raw_bach_df = pd.read_csv(DATA_FILENAME)
+    return raw_bach_df
+
+
+bach_df = get_bach_data()
+
+# ----------------------------------------------------------------------------
+st.title("Bachelorette Analysis")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is a quick analysis of the Bachelorette dataset."
 )
+
+st.link_button("View dataset here",
+               "https://www.kaggle.com/datasets/brianbgonz/the-bachelorette-contestants?resource=download&select=bachelorette-contestants.csv")
+
+st.table(data=bach_df)
